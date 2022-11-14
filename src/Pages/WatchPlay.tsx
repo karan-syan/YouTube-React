9d990fa68c467eb232303f4cb0624b41e5193221
karan-syan <ksyan26@gmail.com>
2022-11-14 13:21:31 +0530

    anotther commit

--- src/Pages/WatchPlay.tsx
+++ src/Pages/WatchPlay.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 
 export default function WatchPlay() {
-  return <div>WatchPlay</div>;
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex mt-1" style={{ height: "92.5vh" }}></div>
+    </div>
+  );
 }
